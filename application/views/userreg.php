2b59a5b5629e2af76bab70ef991e7c1c05d8ded6
kavyab96 <76862194+kavyab96@users.noreply.github.com>
2021-02-24 16:53:57 +0530

    Update userreg.php

--- application/views/userreg.php
+++ application/views/userreg.php
@@ -12,7 +12,7 @@
 <body>
  <form action="<?php echo base_url()?>main/value_insert" method="post">
  	<center><fieldset>
-<<<<<<< HEAD
+
 
   <input type="text" name="fname" placeholder="First name" pattern=".{3,}" required title="3 charecters minimum" maxlength="25"><br><br>
 
@@ -24,15 +24,9 @@
 
   <input type="text" name="mob" placeholder="Mobile number" required minlength="10"maxlength="10"><br><br>
 
-  <input type="email" name="email" placeholder="Email" required><br><br>
-=======
-  <input type="text" name="fname" placeholder="First name"><br><br>
-  <input type="text" name="lname" placeholder="Last name"><br><br>
-  <input type="text" name="uname" placeholder="User name"><br><br>
-  <input type="password" name="pass" placeholder="password"><br><br>
-  <input type="text" name="mob" placeholder="Mobile number"><br><br>
+  
   <input type="email" name="email" placeholder="Email"><br><br>
->>>>>>> a866c6796bb83ead50839027347ec5dd73d4ad12
+
   <input type="submit" name="submit" value="submit">
   
 
@@ -42,4 +36,4 @@
 </form>
 
 </body>
-</html>>
+</html>
